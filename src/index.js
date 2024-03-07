9e861e93da3553cd47874851420a91832b1be8af
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-03-07 18:35:10 +0000

    okay

--- src/index.js
+++ src/index.js
@@ -66,15 +66,20 @@ function Header() {
   );
 }
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
+
       {/* <Pizza
         name="Pizza spinaci"
         ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
@@ -113,9 +118,12 @@ function Footer() {
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open");
   // else alert("sorry weWe're currently closed");
   return (
-    <footer>
+    <footer className="footer">
       {isOpen && (
-        <p>We're open until {closeHour}:00. come visit us or order online.</p>
+        <div className="order">
+          <p>We're open until {closeHour}:00. come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
       )}
     </footer>
   );
