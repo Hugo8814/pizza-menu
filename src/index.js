03976483511b2f72f3c47849d171bf2acef02704
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-03-07 20:36:47 +0000

    done

--- src/index.js
+++ src/index.js
@@ -96,6 +96,7 @@ function Menu() {
   );
 }
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
@@ -117,16 +118,19 @@ function Footer() {
 
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open");
   // else alert("sorry weWe're currently closed");
+  // if (!isOpen)
+  //   return (
+  //     <p>
+  //       {" "}
+  //       we're happy to welcome you between {openHour}:00 and {closeHour}:00
+  //     </p>
+  //   );
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
-          {" "}
           we're happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>
       )}
@@ -134,6 +138,12 @@ function Footer() {
   );
 }
 
+function Order(props) {
+  <div className="order">
+    <p>We're open until {props.closeHour}:00. come visit us or order online.</p>
+    <button className="btn">Order</button>
+  </div>;
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
